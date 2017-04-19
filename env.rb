28f2c305c261a12333e32045e1da7dff011010a1
May Aung <mtaung1s@semo.edu>
2017-04-19 01:06:10 -0700

    Create env.rb

---
+++ env.rb
@@ -0,0 +1,22 @@
+require "capybara"
+require "capybara/cucumber"
+require "rspec"
+require 'capybara/dsl'
+
+include RSpec::Matchers
+
+$timeout = 50
+Capybara.configure do |capybara|
+
+  capybara.register_driver :selenium_ff do |app|
+    capybara::Selenium::Driver.new(app, :browser => :firefox)
+  end
+
+  capybara.default_driver = :selenium_ff #set the browser you want to run the test on
+  capybara.run_server = false
+
+end
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
